07342ed57e9cbb0c89b05ebff45a06ad430c37fd
Potato Holy <holypotato506@gmail.com>
2022-09-19 00:53:34 +0900

    Сделана корзина пользователя

--- Model/Classes/CheckUserRoleClass.cs
+++ Model/Classes/CheckUserRoleClass.cs
@@ -4,8 +4,18 @@ namespace Flowers.Model.Classes
 {
     internal class CheckUserRoleClass
     {
-        public static int CheckUserRole(string Login, string Password) =>
-         CheckLoginClass.CheckLogin(Login, Password) ? TradeEntities.GetContext().User.First(b => b.UserLogin == Login && b.UserPassword == Password).UserRole : 0;
+        public static int CheckUserRole(string Login, string Password)
+        {
+            if (CheckLoginClass.CheckLogin(Login, Password))
+            {
+                UserIDClass.UID = TradeEntities.GetContext().User
+                    .First(b => b.UserLogin == Login && b.UserPassword == Password).UserID;
+                return TradeEntities.GetContext().User.First(b => b.UserLogin == Login && b.UserPassword == Password)
+                    .UserRole;
+            }
+            else
+                return 0;
 
+        }
     }
 }
---
+++ Views/Pages/UserPages/OrderUserPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flowers.Views.Pages.UserPages
+{
+    /// <summary>
+    /// Логика взаимодействия для OrderUserPage.xaml
+    /// </summary>
+    public partial class OrderUserPage : Page
+    {
+        public OrderUserPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
